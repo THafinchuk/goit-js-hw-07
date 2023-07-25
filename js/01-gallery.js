354b6476b98f20ae70cf1a1ac4e89e8417ac252a
THafinchuk <t.hafinchuk@gmail.com>
2023-07-25 11:48:23 +0300

    HW

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,43 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+const galleryEl = document.querySelector('.gallery')
+galleryItems.forEach(e => {
+    galleryEl.innerHTML += `<li class="gallery__item">
+    <a class="gallery__link" href="large-image.jpg">
+      <img
+        class="gallery__image"
+        src="${e.preview}"
+        data-source="${e.original}"
+        alt="${e.description}"
+      />
+    </a>
+  </li>` 
+  
+})
+galleryEl.addEventListener('click',showImg)
+
+   const imgShow =  basicLightbox.create(`
+		<img>
+	`,{
+    onShow: (imgShow) => { 
+      window.addEventListener('keydown', closeImg)
+    },
+    onClose: (imgShow) => {
+      window.removeEventListener('keydown',closeImg)
+    },
+  }
+  )
+function showImg(e){
+  e.preventDefault()
+  imgShow.element().querySelector('img').src = e.target.dataset.source
+  imgShow.show()
+
+}
+function closeImg(e){
+  if(e.code === 'Escape'){
+    imgShow.close()
+}
+}
+
+console.log(galleryItems);
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,16 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+
 console.log(galleryItems);
+
+const galleryEl = document.querySelector('.gallery')
+galleryItems.forEach(e => {
+    galleryEl.innerHTML += `<li class="gallery__item">
+    <a class="gallery__link" href="${e.original}">
+       <img class="gallery__image" src="${e.preview}" alt="${e.description}" />
+    </a>
+ </li>` 
+  
+})
+let lightbox = new SimpleLightbox('.gallery a', {captionsData:'alt',captionDelay:250});
